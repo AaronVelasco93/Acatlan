12202635881336bb7c4b6bb67ce7dea5f218692e
AaronVelasco93 <aaronvelasco1993@outlook.com>
2022-06-15 13:00:52 -0500

    Control de navegador

---
+++ NavigationController/NavigationController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by IOS LAB Acatlan 17 on 15/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func desenlazar(cortar:UIStoryboardSegue){
+        
+    }
+
+}
+
