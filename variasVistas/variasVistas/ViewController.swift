239349c6d9546399819c816a7b4cd1b5c18805c6
AaronVelasco93 <aaronvelasco1993@outlook.com>
2022-06-15 12:28:20 -0500

    Muchas vistas

---
+++ variasVistas/variasVistas/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  variasVistas
+//
+//  Created by IOS LAB Acatlan 17 on 15/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func regresarAPrincipal(unwindSegue:UIStoryboardSegue){
+        
+    }
+
+}
+
